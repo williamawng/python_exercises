62cd814ca131aef6d71260e144c598c611777c2d
William Awng <williammyolattawng@gmail.com>
2023-10-12 18:55:32 -0400

    Strings

--- chapter3.py
+++ chapter3.py
@@ -1,1 +1,26 @@
 # exercises from ch.3
+
+# name = "William Awng"
+first_name = "William"
+last_name = "Awng"
+# name = first_name + " " + last_name
+name = f"{first_name} {last_name}"
+print(name)
+sentence = "I like to play soccer.\nToday is 10\\12\\2023.\nAge\t=\t\"11\""
+print(sentence)
+
+sentence2 = """Hi my name is William.
+I like making friends.
+I am 11 years old.
+Do you wanna be my friend?
+"""
+print(sentence2)
+print("They’ll hibernate during the winter.")
+print("“Absolutely not,” he said.")
+print("\"Absolutely not,\" he said.")
+print("“He said, ’Absolutely not,”’ recalled Mel.")
+print("\"He said, 'Absolutely not,\"' recalled Mel.")
+print("hydrogen sulfide")
+print("left\\right")
+letters = "A\nB\nC"
+print(letters)
