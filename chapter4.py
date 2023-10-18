963da5422a9e4d143af16391c4c1360fad09c3f1
William Awng <williammyolattawng@gmail.com>
2023-10-18 17:48:04 -0400

    Chapter 3 done starting Ch.4

---
+++ chapter4.py
@@ -0,0 +1,13 @@
+import math
+
+# module
+# library
+# pre-defined functions that you can use without having to define it yourself
+
+print(math.pow(2, 3))
+
+
+def pow(x,y):
+    return float(x ** y)
+
+print(pow(2,3))
