98dbb4f368f2474a25cd0ad55977910a85fea677
William Awng <williammyolattawng@gmail.com>
2024-01-17 17:42:19 -0500

    Updated my math_w module and started practicing lists.

--- ch8.py
+++ ch8.py
@@ -11,3 +11,16 @@ for i in range(len(my_list2)):
 for i in range(len(my_list)):
     for j in range(len(my_list2)):
         print(f'{my_list[i], my_list2[j]}')
+
+kingdoms = ['Bacteria', 'Protozoa', 'Chromista', 'Plantae', 'Fungi', 'Animalia']
+print(kingdoms[0])
+print(kingdoms[5])
+print(kingdoms[:3])
+print(kingdoms[2:5])
+print(kingdoms[4:])
+print([])
+print(kingdoms[-6])
+print(kingdoms[-1])
+print(kingdoms[:-3])
+print(kingdoms[-4:-1])
+print(kingdoms[-2:])
--- math_w.py
+++ math_w.py
@@ -1,19 +1,31 @@
 def add(x, y):
+    """ get the sum of a number
+    """
     return x + y
 
 def sub(x, y):
+    """ get the difference of a number
+    """
     return x - y
 
 def multi(x, y):
+    """ get the product of a number
+    """
     return x * y
 
 def divide(x, y):
+    """ get the quotient of a number
+    """
     return x / y
 
 def pow(x, y):
+    """ get the power of a number
+    """
     return x * y
 
 def root(x, y):
+    """ get a root of a number
+    """
     return x ** (1/y)
 
 def remn(x, y):
