08f3a021ac0e59e7b4da28f5a58f4e366dbb30bb
William Awng <williammyolattawng@gmail.com>
2024-01-24 17:56:14 -0500

    I am almost finished chapter 8

--- ch8.py
+++ ch8.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 my_list = ['W', 'i', 'l', 'l', 'i', 'a', 'm']
 
 my_list2 = ["*"]
@@ -31,3 +32,49 @@ print(appointments)
 appointments = appointments + ['16:30']
 print(appointments)
 """The second method is the one that created a new list due to the fact that if you remove 'appointments = ' the function doesn't work."""
+ids = [4353, 2314, 2956, 3382, 9362, 3900]
+# ids.remove(3382)
+# print(ids)
+print(ids.index(9362))
+ids.insert(5, 4499)
+print(ids)
+ids.extend([5566, 1830])
+print(ids)
+ids.reverse()
+print(ids)
+ids.sort(reverse=False)
+print(ids)
+alkaline_earth_metals = [['beryllium', 4], ['magnesium', 12], ['calcium', 20], ['strontium', 38], ['barium', 56], ['radium', 88]]
+print(alkaline_earth_metals)
+print(alkaline_earth_metals[5][1])
+print(len(alkaline_earth_metals))
+alkaline_earth_metals2 = sorted(alkaline_earth_metals, key=itemgetter(1), reverse=True)
+print(alkaline_earth_metals2[0][1])
+temps = [25.2, 16.8, 31.4, 23.9, 28, 22.5, 19.6]
+print(temps)
+temps.sort()
+print(temps)
+low_temps = temps[0:2]
+print(low_temps)
+warm_temps = temps [2:]
+print(warm_temps)
+temps_in_celsius = low_temps + warm_temps
+print(temps_in_celsius)
+
+def same_first_last(L:list) -> bool:
+    if L[0] == L[-1]:
+        return True
+    else:
+        return False
+
+print(same_first_last([3, 4, 2, 8, 4]))
+
+for elem in temps_in_celsius:
+    print(elem)
+
+text = ""
+for i in range(5):
+    for j in range(5):
+        text += "*"
+    text += "\n"
+print(text)
