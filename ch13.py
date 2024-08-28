918aadd7782ddabb908d7699dbda6205a3b0d68e
William Awng <williammyolattawng@gmail.com>
2024-08-28 17:26:09 -0400

    I finished chapter 13(without doing excersises)

--- ch13.py
+++ ch13.py
@@ -1,11 +1,12 @@
 import time
 import random
+count = 1000
 x = ["b", "f", "g", "r", "z", "w", "l"]
 # random randrange takes three arguments (x, y, z)
 # x and y are the two numbers which is the range, x is optional
 # z is the increment, z is also optional
 # the argument in range is how many elements are picked
-y = [random.randrange(1, 1000, 1) for i in range(1000)]
+y = [random.randrange(1, count, 1) for i in range(count)]
 y.sort()
 target = 456
 def linear_search(l, target):
@@ -66,7 +67,7 @@ def selectionSort(array):
          # swapping the elements to sort the array
         (array[ind], array[min_index]) = (array[min_index], array[ind])
 
-y = [random.randrange(1, 1000, 1) for i in range(1000)]
+y = [random.randrange(1, count, 1) for i in range(count)]
 # print(y)
 t1 = time.perf_counter()
 selectionSort(y)
@@ -91,7 +92,7 @@ def insertionSort(arr):
             j -= 1
         arr[j+1] = key  # Insert the key in the correct position
 
-y = [random.randrange(1, 1000, 1) for i in range(1000)]
+y = [random.randrange(1, count, 1) for i in range(count)]
 # print(y)
 t1 = time.perf_counter()
 insertionSort(y)
@@ -99,4 +100,67 @@ t2 = time.perf_counter()
 t = (t2 - t1) * 1000
 print(f"Insertion sort took {t}ms ")
 # print(y)
+
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = left  # Initial index of merged subarray
+
+    # Merge the temp arrays back
+    # into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[],
+    # if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+arr = [random.randrange(1, count, 1) for i in range(count)]
+
+# print(arr)
+
+t1 = time.perf_counter()
+merge_sort(arr, 0, len(arr) - 1)
+t2 = time.perf_counter()
+t = (t2 - t1) * 1000
+print(f"Merge sort took {t}ms ")
+
+# print(arr)
 
